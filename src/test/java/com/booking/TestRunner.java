b173cff9b60e3c1fb891b0b69fcad71dad8903b2
ADITYA B <abad236@gmail.com>
2026-07-03 22:48:58 +0530

    feat: Introduced parallel execution feature

--- src/test/java/com/booking/TestRunner.java
+++ src/test/java/com/booking/TestRunner.java
@@ -1,6 +1,7 @@
 package com.booking;
 
 import org.junit.platform.suite.api.*;
+
 import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 
@@ -10,5 +11,8 @@ import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.booking")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports.html, json:target/cucumber.json")
+@ConfigurationParameter(key = "cucumber.execution.parallel.enabled", value = "true")
+@ConfigurationParameter(key = "cucumber.execution.parallel.config.strategy", value = "fixed")
+@ConfigurationParameter(key = "cucumber.execution.parallel.config.fixed.parallelism", value = "2")
 public class TestRunner {
 }
